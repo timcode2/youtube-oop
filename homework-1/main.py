3c094d78515547321ce2596c70df4aa1baa091e3
artemm <artem.m2013@gmail.com>
2023-06-12 17:24:27 +0300

    don't see API key

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,8 +1,8 @@
 from src.channel import Channel
 
 if __name__ == '__main__':
-    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-    moscowpython.print_info()
+    rozetked = Channel('UCDF_NIAEkcAUvzxe1DUzaQA')
+    rozetked.print_info()
 
     """
 {
