5f84d69e55f152a46f1283c2a5bab77154955d2c
artemm <artem.m2013@gmail.com>
2023-06-14 16:26:25 +0300

    add get_service and to_json

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,15 +1,15 @@
 from src.channel import Channel
 
 if __name__ == '__main__':
-    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    rozetked = Channel('UCDF_NIAEkcAUvzxe1DUzaQA')
 
     # получаем значения атрибутов
-    print(moscowpython.title)  # MoscowPython
-    print(moscowpython.video_count)  # 685 (может уже больше)
-    print(moscowpython.url)  # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
+    print(rozetked.title)  # MoscowPython
+    print(rozetked.video_count)  # 685 (может уже больше)
+    print(rozetked.url)  # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
 
     # менять не можем
-    moscowpython.channel_id = 'Новое название'
+    rozetked.channel_id = 'Новое название'
     # AttributeError: property 'channel_id' of 'Channel' object has no setter
 
     # можем получить объект для работы с API вне класса
@@ -17,4 +17,4 @@ if __name__ == '__main__':
     # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
 
     # создаем файл 'moscowpython.json' в данными по каналу
-    moscowpython.to_json('moscowpython.json')
+    rozetked.to_json('moscowpython.json')
--- src/channel.py
+++ src/channel.py
@@ -2,19 +2,43 @@ import json
 import os
 from googleapiclient.discovery import build
 
-api_key: str = os.getenv("YT_API_KEY")
-
-youtube = build('youtube', 'v3', developerKey=api_key)
-
 class Channel:
     """Класс для ютуб-канала"""
 
+    api_key: str = os.getenv("YT_API_KEY")
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        self.title = self.get_info()['items'][0]['snippet']['title']
+        self.description = self.get_info()['items'][0]['snippet']['localized']['description']
+        self.url = ''.join(['https://www.youtube.com/channel/', self.__channel_id])
+        self.subscriber_count = self.get_info()['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.get_info()['items'][0]['statistics']['videoCount']
+        self.view_count = self.get_info()['items'][0]['statistics']['viewCount']
 
+    def get_info(self):
+        channel = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        return channel
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        print(json.dumps(self.get_info(), indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=cls.api_key)
+
+    def to_json(self, file_name):
+        data = {
+            'channel_id': self.__channel_id,
+            'title': self.title,
+            'description': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'video_count': self.video_count,
+            'view_count': self.view_count
+        }
+
+        with open(file_name, 'w', encoding='utf-8') as f:
+            json.dump(data, f, indent=2, ensure_ascii=False)
