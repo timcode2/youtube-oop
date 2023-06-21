dce36ab465db5d79598322bbef305c47c20f4208
artemm <artem.m2013@gmail.com>
2023-06-21 04:13:57 +0300

    fix class Video in video.py

--- src/video.py
+++ src/video.py
@@ -7,14 +7,17 @@ class Video:
     api_key: str = os.getenv('YT_API_KEY')
 
     def __init__(self, video_id):
-        self.__video_id = video_id
+        self.video_id = video_id
         self.video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                                id=video_id
                                                                ).execute()
         self.title: str = self.video_response['items'][0]['snippet']['title']
-        self.url = ''.join(['https://www.youtube.com/watch?v=', self.__video_id])
+        self.url = ''.join(['https://www.youtube.com/watch?v=', self.video_id])
         self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
         self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return f'{self.title}'
 
     def get_info(self):
         return self.video_response
@@ -27,9 +30,11 @@ class Video:
     def get_service(cls):
         return build('youtube', 'v3', developerKey=cls.api_key)
 
-class PLVideo:
-    pass
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
 
 
-vid1 = Video('AWX4JnAnjBE')
-print(vid1)
+
+
