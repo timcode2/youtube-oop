2e85438026b7ca1916aab4af94ec423c3a2d2141
artemm <artem.m2013@gmail.com>
2023-07-02 16:06:25 +0300

    fix __init__ for class PLVideo

--- src/video.py
+++ src/video.py
@@ -33,12 +33,7 @@ class Video:
 class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
-        self.playlist_id = self.get_service().playlistItems().list(playlistId=playlist_id,
-                                               part='contentDetails',
-                                               maxResults=50,
-                                               ).execute()
+        self.playlist_id = playlist_id
 
     def __str__(self):
         return f'{self.title}'
-
-
